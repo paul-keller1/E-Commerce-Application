8cba8cf4476fa8db890de1433ec4d465432b96d4
scoro <paul.keller@online.de>
2025-12-02 17:22:51 +0100

    -Revised tests for CartServiceImpl and CategoryServiceImpl to achieve 100% jacoco coverage

--- ECommerceApplication/src/test/java/com/app/services/CartServiceImplTest.java
+++ ECommerceApplication/src/test/java/com/app/services/CartServiceImplTest.java
@@ -65,8 +65,6 @@ public class CartServiceImplTest {
         product.setDiscount(10);
         product.setQuantity(10);
 
-
-
         cartItem = new CartItem();
         cartItem.setCartItemId(1L);
         cartItem.setCart(cart);
@@ -83,9 +81,9 @@ public class CartServiceImplTest {
 
     @Test
     void testAddProductToCart_Success() {
-        when(cartRepo.findById(1L)).thenReturn(Optional.of(cart));
-        when(productRepo.findById(1L)).thenReturn(Optional.of(product));
-        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(null);
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
+        when(productRepo.findById(product.getProductId())).thenReturn(Optional.of(product));
+        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(Optional.empty());
 
         CartDTO result = cartService.addProductToCart(cart.getCartId(), product.getProductId(), 2);
 
@@ -96,9 +94,9 @@ public class CartServiceImplTest {
 
     @Test
     void testAddProductToCart_ProductAlreadyInCart() {
-        when(cartRepo.findById(1L)).thenReturn(Optional.of(cart));
-        when(productRepo.findById(1L)).thenReturn(Optional.of(product));
-        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(cartItem);
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
+        when(productRepo.findById(product.getProductId())).thenReturn(Optional.of(product));
+        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(Optional.ofNullable(cartItem));
 
         assertThrows(APIException.class, () -> cartService.addProductToCart(cart.getCartId(), product.getProductId(), 1));
     }
@@ -107,23 +105,54 @@ public class CartServiceImplTest {
     void testAddProductToCart_ProductOutOfStock() {
         product.setQuantity(0);
 
-        when(cartRepo.findById(1L)).thenReturn(Optional.of(cart));
-        when(productRepo.findById(1L)).thenReturn(Optional.of(product));
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
+        when(productRepo.findById(product.getProductId())).thenReturn(Optional.of(product));
 
 
-        assertThrows(APIException.class, () -> cartService.addProductToCart(1L, 1L, 1));
+        assertThrows(APIException.class, () -> cartService.addProductToCart(cart.getCartId(), product.getProductId(), 1));
     }
 
     @Test
     void testAddProductToCart_QuantityTooHigh(){
 
-        when(cartRepo.findById(1L)).thenReturn(Optional.of(cart));
-        when(productRepo.findById(1L)).thenReturn(Optional.of(product));
-        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(null);
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
+        when(productRepo.findById(product.getProductId())).thenReturn(Optional.of(product));
+        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(Optional.empty());
 
 
         assertThrows(APIException.class, () -> cartService.addProductToCart(cart.getCartId(), product.getProductId(), 11));
     }
+
+    @Test
+    void testAddProductToCart_CartNotFound() {
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.empty());
+
+        APIException ex = assertThrows(
+                APIException.class,
+                () -> cartService.addProductToCart(cart.getCartId(), product.getProductId(), 1)
+        );
+
+        String msg = ex.getMessage();
+        assertTrue(msg.contains("Cart with cartId"));
+        assertTrue(msg.contains("was not found!!!"));
+    }
+
+
+    @Test
+    void testAddProductToCart_ProductNotFoundById() {
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
+        when(productRepo.findById(product.getProductId())).thenReturn(Optional.empty());
+
+        APIException ex = assertThrows(
+                APIException.class,
+                () -> cartService.addProductToCart(cart.getCartId(), product.getProductId(), 1)
+        );
+
+        String expected = "Product with productId" + product.getProductId() + "was not found!!!";
+        assertEquals(expected, ex.getMessage());
+    }
+
+
 
 
     // ---------------------------------------------------------
@@ -144,8 +173,15 @@ public class CartServiceImplTest {
     @Test
     void testGetAllCarts_NoCarts() {
         when(cartRepo.findAll()).thenReturn(Collections.emptyList());
-        assertThrows(APIException.class, () -> cartService.getAllCarts());
+
+        APIException ex = assertThrows(
+                APIException.class,
+                () -> cartService.getAllCarts()
+        );
+
+        assertEquals("No cart exists!!!", ex.getMessage());
     }
+
 
     // ---------------------------------------------------------
     // 3. getCart
@@ -163,8 +199,16 @@ public class CartServiceImplTest {
     @Test
     void testGetCart_NotFound() {
         when(cartRepo.findCartByEmailAndCartId("test@test.com", cart.getCartId())).thenReturn(null);
-        assertThrows(ResourceNotFoundException.class, () -> cartService.getCart("test@test.com", cart.getCartId()));
+
+        APIException ex = assertThrows(
+                APIException.class,
+                () -> cartService.getCart("test@test.com", cart.getCartId())
+        );
+
+        String expected = "Cart with cartId" + cart.getCartId() + "was not found!!!";
+        assertEquals(expected, ex.getMessage());
     }
+
 
     // ---------------------------------------------------------
     // 4. updateProductInCarts
@@ -174,7 +218,7 @@ public class CartServiceImplTest {
     void testUpdateProductInCarts_Success() {
         when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
         when(productRepo.findById(product.getProductId())).thenReturn(Optional.of(product));
-        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(cartItem);
+        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(Optional.ofNullable(cartItem));
         when(cartItemRepo.save(any())).thenReturn(cartItem);
 
         assertDoesNotThrow(() -> cartService.updateProductInCarts(cart.getCartId(), product.getProductId()));
@@ -184,10 +228,40 @@ public class CartServiceImplTest {
     void testUpdateProductInCarts_ProductNotInCart() {
         when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
         when(productRepo.findById(product.getProductId())).thenReturn(Optional.of(product));
-        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(null);
+        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(Optional.empty());
 
         assertThrows(APIException.class, () -> cartService.updateProductInCarts(cart.getCartId(), product.getProductId()));
     }
+
+    @Test
+    void testUpdateProductInCarts_CartNotFound() {
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.empty());
+
+        APIException ex = assertThrows(
+                APIException.class,
+                () -> cartService.updateProductInCarts(cart.getCartId(), product.getProductId())
+        );
+
+        String expected = "Cart with cartId" + cart.getCartId()  + "was not found!!!";
+        assertEquals(expected, ex.getMessage());
+    }
+
+
+    @Test
+    void testUpdateProductInCarts_ProductNotFound() {
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
+        when(productRepo.findById(product.getProductId())).thenReturn(Optional.empty());
+
+        APIException ex = assertThrows(
+                APIException.class,
+                () -> cartService.updateProductInCarts(cart.getCartId(), product.getProductId())
+        );
+
+        String expected = "Product with productId" + product.getProductId() + "was not found!!!";
+        assertEquals(expected, ex.getMessage());
+    }
+
+
 
     // ---------------------------------------------------------
     // 5. updateProductQuantityInCart
@@ -197,12 +271,13 @@ public class CartServiceImplTest {
     void testUpdateProductQuantityInCart_Success() {
         when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
         when(productRepo.findById(product.getProductId())).thenReturn(Optional.of(product));
-        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(cartItem);
+        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(Optional.ofNullable(cartItem));
         when(cartItemRepo.save(any())).thenReturn(cartItem);
 
         CartDTO result = cartService.updateProductQuantityInCart(cart.getCartId(), product.getProductId(), 3);
 
-        CartItem cartItemUpdated = cartItemRepo.findCartItemByProductIdAndCartId(result.getCartId(), result.getProducts().get(0).getProductId());
+        CartItem cartItemUpdated = cartItemRepo.findCartItemByProductIdAndCartId(result.getCartId(), result.getProducts().get(0).getProductId())
+                .orElseThrow(() -> new APIException("Test function: cartItem doesnt exist!!!"));
         assertEquals(3, cartItemUpdated.getQuantity());
     }
 
@@ -234,11 +309,40 @@ public class CartServiceImplTest {
     void testUpdateProductQuantityInCart_ProductNotAvailableInCart() {
         product.setQuantity(1);
 
-        when(cartRepo.findById(1L)).thenReturn(Optional.of(cart));
-        when(productRepo.findById(1L)).thenReturn(Optional.of(product));
-        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(null);
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
+        when(productRepo.findById(product.getProductId())).thenReturn(Optional.of(product));
+        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(Optional.empty());
 
         assertThrows(APIException.class, () -> cartService.updateProductQuantityInCart(cart.getCartId(), product.getProductId(), 1));
+    }
+
+
+    @Test
+    void testUpdateProductQuantityInCart_CartNotFound() {
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.empty());
+
+        APIException ex = assertThrows(
+                APIException.class,
+                () -> cartService.updateProductQuantityInCart(cart.getCartId(), product.getProductId(), 1)
+        );
+
+        String expected = "Cart with cartId" + cart.getCartId() + "was not found!!!";
+        assertEquals(expected, ex.getMessage());
+    }
+
+
+    @Test
+    void testUpdateProductQuantityInCart_ProductNotFound() {
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
+        when(productRepo.findById(product.getProductId())).thenReturn(Optional.empty());
+
+        APIException ex = assertThrows(
+                APIException.class,
+                () -> cartService.updateProductQuantityInCart(cart.getCartId(), product.getProductId(), 1)
+        );
+
+        String expected = "Product with productId" + product.getProductId() + "was not found!!!";
+        assertEquals(expected, ex.getMessage());
     }
 
 
@@ -250,8 +354,8 @@ public class CartServiceImplTest {
 
     @Test
     void testDeleteProductFromCart_Success() {
-        when(cartRepo.findById(1L)).thenReturn(Optional.of(cart));
-        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(cartItem);
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
+        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(Optional.ofNullable(cartItem));
 
         String message = cartService.deleteProductFromCart(cart.getCartId(), product.getProductId());
 
@@ -261,10 +365,34 @@ public class CartServiceImplTest {
 
     @Test
     void testDeleteProductFromCart_ItemNotFound() {
-        when(cartRepo.findById(1L)).thenReturn(Optional.of(cart));
-        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(null);
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.of(cart));
+        when(cartItemRepo.findCartItemByProductIdAndCartId(cart.getCartId(), product.getProductId())).thenReturn(Optional.empty());
 
-        assertThrows(ResourceNotFoundException.class,
-                () -> cartService.deleteProductFromCart(cart.getCartId(), product.getProductId()));
+        APIException ex = assertThrows(
+                APIException.class,
+                () -> cartService.deleteProductFromCart(cart.getCartId(), product.getProductId())
+        );
+
+        String msg = ex.getMessage();
+        String expected = "Cart item with cartId" + cart.getCartId()  + "and productId" + product.getProductId() + "was not found!!!";
+        assertEquals(expected, msg);
     }
+
+
+    @Test
+    void testDeleteProductFromCart_CartNotFound() {
+        when(cartRepo.findById(cart.getCartId())).thenReturn(Optional.empty());
+
+        APIException ex = assertThrows(
+                APIException.class,
+                () -> cartService.deleteProductFromCart(cart.getCartId(), product.getProductId())
+        );
+
+        String msg = ex.getMessage();
+        String expected = "Cart with cartId" + cart.getCartId() + "was not found!!!";
+
+    }
+
+
+
 }
