5e31675f45e3119f1e90cec5a90dcfb4031fb5b4
scoro <paul.keller@online.de>
2025-12-03 21:11:45 +0100

    UserDetaServiceImplTest 100% coverage

--- ECommerceApplication/src/main/java/com/app/services/UserDetailsServiceImpl.java
+++ ECommerceApplication/src/main/java/com/app/services/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.app.services;
 
 import java.util.Optional;
 
+import com.app.exceptions.APIException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,7 +11,6 @@ import org.springframework.stereotype.Service;
 
 import com.app.config.UserInfoConfig;
 import com.app.entites.User;
-import com.app.exceptions.ResourceNotFoundException;
 import com.app.repositories.UserRepo;
 
 @Service
@@ -19,10 +19,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	@Autowired
 	private UserRepo userRepo;
 
+
+	//TODO: maybe i should change sth about the way tokens return emails instead of usernames, loadUserByUsername is from a remote interface
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Optional<User> user = userRepo.findByEmail(username);
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<User> user = userRepo.findByEmail(email);
 		
-		return user.map(UserInfoConfig::new).orElseThrow(() -> new ResourceNotFoundException("User", "email", username));
+		return user.map(UserInfoConfig::new).orElseThrow(() -> new APIException("User with email" + email + "not found!!!"));
 	}
 }
