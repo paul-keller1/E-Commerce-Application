20a2095b4fd024a9efacc50e0d0b148b9e7b1106
scoro <paul.keller@online.de>
2025-12-12 19:52:26 +0100

    Docker -use maven version with few known vulnerabilities to create debian image -use openjdk at runtime -replaced passwordencoder and included unencrypted default users

--- ECommerceApplication/src/main/java/com/app/config/SecurityConfig.java
+++ ECommerceApplication/src/main/java/com/app/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -63,7 +64,7 @@ public class SecurityConfig {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
 	@Bean
